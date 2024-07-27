fb6a280895d153b4299cdfcefadf665e419a5ae8
NotArchon <notarchon@gmail.com>
2024-07-27 10:37:52 -0700

    add link to java standard options to example run scripts

--- releases/unix/example-run.sh
+++ releases/unix/example-run.sh
@@ -9,5 +9,5 @@ export config_path="../example-config.toml"
 # The path to the java executable file (optional)
 export java_exe_path=""
 
-# Source the run_bot.sh script with the specified VM arguments
+# Source the run_bot.sh script with the specified VM arguments (https://docs.oracle.com/en/java/javase/22/docs/specs/man/java.html#standard-options-for-java)
 . ./lib/run_bot.sh -Xmx1024m
--- releases/windows/example-run.bat
+++ releases/windows/example-run.bat
@@ -9,5 +9,5 @@ set "config_path=..\example-config.toml"
 rem The path to the java exe file (optional)
 set "java_exe_path="
 
-rem Call the run_bot.bat script with the specified VM arguments
+rem Call the run_bot.bat script with the specified VM arguments (https://docs.oracle.com/en/java/javase/22/docs/specs/man/java.html#standard-options-for-java)
 call ".\lib\run_bot.bat" -Xmx1024m
