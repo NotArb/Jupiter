5b2e2345303d72a6a914e6f9981a8eca178257d4
NotArchon <notarchon@gmail.com>
2024-08-03 13:43:58 -0700

    Release Update: 0.1.18-alpha

--- releases/unix-notarb/run.sh
+++ releases/unix-notarb/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The path to the bot file (required)
-export bot_path="../NotArb-0.1.17-alpha"
+export bot_path="../NotArb-0.1.18-alpha"
 
 # The path to the bot config file (required)
 export config_path="../temp-config.toml"
