4800228a16bb856a29ee7aad073f04eb42e1cc03
NotArchon <notarchon@gmail.com>
2024-08-03 09:09:36 -0700

    Release Update: 0.1.17-alpha

--- releases/windows-notarb/run.bat
+++ releases/windows-notarb/run.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem The path to the bot file (required)
-set "bot_path=..\NotArb-0.1.16-alpha"
+set "bot_path=..\NotArb-0.1.17-alpha"
 
 rem The path to the bot config file (required)
 set "config_path=..\temp-config.toml"
