6fd2d39b7ca9fd30029cdbc11bd1be353747bda8
NotArchon <notarchon@gmail.com>
2024-08-06 19:08:33 -0700

    changes for docker-jupiter setup

--- releases/docker-jupiter/build.sh
+++ releases/docker-jupiter/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# If you want your Jupiter server to listen on a different port, change this before building.
+# This script depends on the default release packaging.
+# Specifically, the "notarb-*" bot file should be one directory up from this script's directory.
+# If you move the bot file, you will need to change the build context path, which is currently set to: ..
+
+# To change the port on which your Jupiter server listens, modify the value of PORT before building.
+# Note: Ports must be mapped when creating Docker containers, so this change needs to be made here.
 PORT=8080
 
 # Ensure Docker is installed
@@ -23,16 +28,14 @@ if [ -z "$vm_args" ]; then
   vm_args="-Xmx512m"
 fi
 
-# Get the absolute directory of the current script
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-
-# Move up one directory from the script's directory
-cd "$script_dir/.."
+# Move to the correct workdir to prevent path issues
+cd "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Build docker image
 docker build -t jupiter-image:latest \
   --build-arg VM_ARGS="$vm_args" \
-  -f "$script_dir/Dockerfile" .
+  --no-cache \
+  -f "./Dockerfile" ..
 
 echo "Jupiter image created!"
 
@@ -42,7 +45,7 @@ docker stop jupiter 2>/dev/null || true
 docker rm jupiter 2>/dev/null || true
 docker create --name jupiter --restart unless-stopped \
   -p $PORT:8080 \
-  --mount type=bind,source="$script_dir/mount",destination=/jupiter/mount \
+  --mount type=bind,source="./mount",destination="/jupiter/mount" \
   jupiter-image:latest
 
 echo "Jupiter container created!"
@@ -53,5 +56,7 @@ echo "You can now run your Jupiter server with the following command:"
 echo "-> docker start jupiter"
 echo "Here are some other useful commands:"
 echo "-> docker stop jupiter"
+echo "-> docker restart jupiter"
+echo "-> docker stats jupiter"
 echo "-> docker logs --follow jupiter"
 echo '-> docker update --cpus="123456" jupiter'
