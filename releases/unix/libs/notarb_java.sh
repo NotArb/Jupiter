434f4e70f83784abeae33adff0fa3347612860f8
NotArchon <notarchon@gmail.com>
2024-10-09 12:20:57 -0700

    Release Update: 0.1.69-alpha

--- releases/unix/libs/notarb_java.sh
+++ releases/unix/libs/notarb_java.sh
@@ -4,7 +4,7 @@
 libs_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # The path to the bot file (required)
-bot_path="$libs_path/notarb-0.1.68-alpha.jar"
+bot_path="$libs_path/notarb-0.1.69-alpha.jar"
 
 # Ensure bot_path file exists
 if [ ! -f "$bot_path" ]; then
